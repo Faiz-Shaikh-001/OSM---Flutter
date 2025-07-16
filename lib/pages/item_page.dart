c321441abfdf67d3116db4ede8503c5b371b699d
Faiz Shaikh <faizshk008@gmail.com>
2025-07-16 17:58:20 +0530

    Implemented ItemPage

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -53,6 +53,30 @@ class ItemPage extends StatelessWidget {
               fit: BoxFit.cover,
               width: 400,
               height: 300,
+              loadingBuilder:
+                  (
+                    BuildContext context,
+                    Widget child,
+                    ImageChunkEvent? loadingProgress,
+                  ) {
+                    if (loadingProgress == null) return child;
+                    return Center(
+                      child: CircularProgressIndicator(
+                        value: loadingProgress.expectedTotalBytes != null
+                            ? loadingProgress.cumulativeBytesLoaded /
+                                  loadingProgress.expectedTotalBytes!
+                            : null,
+                      ),
+                    );
+                  },
+              errorBuilder:
+                  (
+                    BuildContext context,
+                    Object exception,
+                    StackTrace? stackTrace,
+                  ) {
+                    return Text('Error loading image!');
+                  },
             ),
             SafeArea(
               child: Padding(
