6f7420f9f0b86e75d49ff9c68f8e0a4d4e7343bb
Thaufeeq Ahamed <thaufeeqahamednew@gmail.com>
2026-03-25 03:19:44 +0530

    feat: Add Google Services configuration and apply Google Services plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("com.google.gms.google-services") version "4.3.15" apply false
+}
+
 allprojects {
     repositories {
         google()
@@ -15,7 +19,6 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
 }
-
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
